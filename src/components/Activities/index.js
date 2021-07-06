f457ee008ea21ac264a4d84a681526715cd28fd7
marcosbarker <marcos_barker@hotmail.com>
2021-07-06 19:45:42 -0300

    fix: errorRenderActiviti

--- src/components/Activities/index.js
+++ src/components/Activities/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Container, Header, Title } from './style';
+import {CardFooter, Value, Details, Container, Header, Card, CardHeader, 
+        Title, Avatar, Description,  Bold, CardBody, UserName} from './style';
+
+import avatar from '../../assets/images/avatar.png'
 
 export default function Activities() {
   return (
@@ -7,6 +10,28 @@ export default function Activities() {
       <Header>
         <Title>Atividades</Title>
       </Header>
+
+      <Card>
+        <CardHeader>
+          <Avatar 
+            source={avatar}
+          />
+          <Description>
+            <Bold>Você</Bold> pagou a <Bold>@Cornelius</Bold>
+          </Description>
+        </CardHeader>
+
+        <CardBody>
+          <UserName>Marcos Barker</UserName>
+        </CardBody>
+
+        <CardFooter>
+          <Details>
+            <Value>R$ 387,00</Value>
+          </Details>
+        </CardFooter>
+
+      </Card>
     </Container>
   );
 }
--- src/components/Activities/style.js
+++ src/components/Activities/style.js
@@ -6,7 +6,18 @@ export const Container = styled.View`
 `;
 
 export const Header = styled.View`
-  justify-content: center;
+`;
+
+export const Avatar = styled.Image`
+`;
+
+export const CardBody = styled.View`
+  margin-top: 20px;
+`;
+
+export const UserName = styled.Text`
+  font-size: 20px;
+  color: white;
 `;
 
 export const Title = styled.Text`
@@ -14,3 +25,31 @@ export const Title = styled.Text`
   font-weight: bold;
   color: white;
 `;
+
+export const Description = styled.Text`
+  color: white;
+  font-size: 16px;
+  margin-left: 15px;
+`;
+
+export const Bold = styled.Text`
+  font-weight: bold;
+`;
+
+export const Card = styled.View`
+  background: #1e222b;
+  border-radius: 8px;
+  margin-top: 20px;
+  padding: 15px;
+`;
+
+export const CardHeader = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const CardFooter = styled.View``;
+
+export const Details = styled.View``;
+
+export const Value = styled.Text``;
