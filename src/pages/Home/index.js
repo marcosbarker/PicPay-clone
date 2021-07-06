fd66ccdee5aca2d6e4dc075220a2d90d4a846072
marcosbarker <marcos_barker@hotmail.com>
2021-07-06 10:09:07 -0300

    feat: headerConfigIcons

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { Wrapper } from './style';
+import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons'
+import { Wrapper, Header, Balance, BalanceTitle, BalanceContainer } from './style';
 
 export default function Home() {
   return (
     <Wrapper>
-      <Text>Home</Text>
+      <Header>
+        <MaterialCommunityIcons
+          name="qrcode-scan"
+          size={30}
+          color="#10c86c"
+        />
+      <BalanceContainer>
+        <BalanceTitle>Meu Saldo</BalanceTitle>
+        <Balance>R$ 0,00</Balance>
+      </BalanceContainer>
+
+      <AntDesign
+          name="gift"
+          size={30}
+          color="#10c86c"
+        />
+      </Header>
     </Wrapper>
   );
 }
