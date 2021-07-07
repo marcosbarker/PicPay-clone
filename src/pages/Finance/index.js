ec3d96b326b46ca81b1e61d4e17026df008aea55
marcosbarker <marcos_barker@hotmail.com>
2021-07-07 03:54:06 -0300

    feat: create cardCartAndCardPlus

--- src/pages/Finance/index.js
+++ src/pages/Finance/index.js
@@ -1,55 +1,102 @@
 import React from 'react';
 import { Switch } from 'react-native';
-import { Feather, FontAwesome5, FontAwesome } from '@expo/vector-icons';
-import { UseBalance, UseBalanceTitle, Action, Actions, ActionLabel, Info, SecretButton, BalanceContainer, Bold, Value, Wrapper, HeaderContainer, Header, Title } from './style';
+import { Container, AddButton, AddButton2, AddLabel, CardBody, Img, CardInfo, 
+         CardDetails, CardLabel, CardTitle, Card, FormsPayTitle, 
+         FormsPay, UseBalance, UseBalanceTitle, Action, Actions, 
+         ActionLabel, Info, SecretButton, BalanceContainer, Bold, 
+         Value, Wrapper, HeaderContainer, Header, Title } from './style';
+import credicard from '../../assets/images/credit-card.png'
+import { Feather, FontAwesome5, FontAwesome, AntDesign } from '@expo/vector-icons';
 
 export default function Finance() {
   return (
     <Wrapper>
-      <Header colors={['#52E78C', '#1AB563']}>
-        <HeaderContainer>
-          <Title>Saldo disponível</Title>
-          <BalanceContainer>
-            <Value>
-              R$ <Bold>228.000,00</Bold>
-            </Value>
-            <SecretButton> 
-              <Feather
-                name="eye"
-                size={28}
-                color="white"
-              />
-            </SecretButton>
-          </BalanceContainer>
-          <Info>
-            Seu saldo está rendendo 130% do CDI. &gt;
-          </Info>
-          <Actions>
-            <Action>
-              <FontAwesome5 
-                name="piggy-bank"
-                size={20}
-                color="white"             
-              />
-            <ActionLabel>Adicionar dinheiro</ActionLabel>
-            </Action>
-             <Action>
+      <Container>
+        <Header colors={['#52E78C', '#1AB563']}>
+          <HeaderContainer>
+            <Title>Saldo disponível</Title>
+            <BalanceContainer>
+              <Value>
+                R$ <Bold>228.000,00</Bold>
+              </Value>
+              <SecretButton> 
+                <Feather
+                  name="eye"
+                  size={28}
+                  color="white"
+                />
+              </SecretButton>
+            </BalanceContainer>
+            <Info>
+              Seu saldo está rendendo 130% do CDI. &gt;
+            </Info>
+            <Actions>
+              <Action>
+                <FontAwesome5 
+                  name="piggy-bank"
+                  size={20}
+                  color="white"             
+                />
+              <ActionLabel>Adicionar dinheiro</ActionLabel>
+              </Action>
+              <Action>
+                <FontAwesome
+                  name="bank"
+                  size={20}
+                  color="white"             
+                />
+              <ActionLabel>Retirar dinheiro</ActionLabel>
+              </Action>
+            </Actions>
+          </HeaderContainer>
+        </Header>
+        <UseBalance>
+          <UseBalanceTitle>
+            Usar saldo em carteira ao pagar
+          </UseBalanceTitle>
+          <Switch />
+        </UseBalance>
+        <FormsPay>
+          <FormsPayTitle>
+            Fomras de pagamento
+          </FormsPayTitle>
+          <Card>
+            <CardBody>
+            <Img source={credicard} resizeMode="contain"/>
+            <CardDetails>
+              <CardTitle>
+                Visa
+              </CardTitle>
+              <CardInfo>
+                Cartão com final ****4789
+              </CardInfo>
+            </CardDetails>
+            </CardBody>
+            <AddButton>
               <FontAwesome
-                name="bank"
-                size={20}
-                color="white"             
-              />
-            <ActionLabel>Retirar dinheiro</ActionLabel>
-            </Action>
-          </Actions>
-        </HeaderContainer>
-      </Header>
-      <UseBalance>
-        <UseBalanceTitle>
-          Usar saldo em carteira ao pagar
-        </UseBalanceTitle>
-        <Switch />
-      </UseBalance>
+                name="exclamation-circle"
+                size={24}
+                color="#1AB563"           
+            />
+            <AddLabel>
+              Toque para verificar seu cartão
+            </AddLabel>
+            </AddButton>
+          </Card>
+          <Card>
+            <AddButton2>
+            <AntDesign 
+              name="pluscircleo"
+              size={24}
+              color="#1AB563"
+            />
+            <CardLabel>
+              Adicionar Cartão de crédito
+            </CardLabel>
+            </AddButton2>
+          </Card>
+        </FormsPay>
+      </Container>
     </Wrapper>
   );
 }
--- src/pages/Finance/style.js
+++ src/pages/Finance/style.js
@@ -6,6 +6,10 @@ export const Wrapper = styled.View`
   flex: 1;
 `;
 
+//rolagem da pagina
+export const Container = styled.ScrollView`
+`;
+
 export const Header = styled(LinearGradient)`
   height: 300px;
 `;
@@ -84,5 +88,76 @@ export const UseBalance = styled.View`
 
 export const UseBalanceTitle = styled.Text`
   color: white;
+  font-size: 17px;
+`;
+
+
+export const FormsPay = styled.View`
+  padding: 1px 16px 1px 16px;
+  margin-top: 20px;
+`;
+
+export const FormsPayTitle = styled.Text`
+  text-transform: uppercase;
+  color: #8E8E93; 
+  font-weight: bold;
+  font-size: 16px;
+`;
+
+export const Card = styled.View`
+  border-radius: 8px;
+  padding: 10px;
+  margin-top: 10px;
+  background: #1E232A;
+`;
+
+export const CardBody = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const CardTitle = styled.Text`
+  color: white;
   font-size: 18px;
+`; 
+
+export const CardLabel = styled.Text`
+  color: #1AB563;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+  margin-left: 10px;
+`;
+
+export const CardDetails = styled.View`
+  flex: 1;
+`;
+
+export const CardInfo = styled.Text`
+  font-size: 16px;
+  color: rgba(255, 255, 255, 0.8);
+  margin-top: 1px;
+`;
+
+export const Img = styled.Image`
+ width: 50px;
+ margin-left: 6px;
+ margin-right: 15px;
+`;
+
+export const AddLabel = styled.Text`
+  color: #1AB563;
+  margin-left: 10px;
+`;
+
+export const AddButton = styled.TouchableOpacity`
+  margin: 1px 0 0 6px;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const AddButton2 = styled.TouchableOpacity`
+  margin: 10px 6px 10px 6px;
+  align-items: center;
+  flex-direction: row;
 `;
