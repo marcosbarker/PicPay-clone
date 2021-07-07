dff227618c0a5616fa48514bb0654ef48ecdb641
marcosbarker <marcos_barker@hotmail.com>
2021-07-07 15:04:36 -0300

    chore: refactor

--- src/Navigation.js
+++ src/Navigation.js
@@ -31,7 +31,7 @@ const icons = {
 export default function Navigation() {
   return (
     <Tab.Navigator
-      initialRouteName="Finance"
+      initialRouteName=""
       screenOptions={({route, navigation}) => ({
         tabBarIcon: ({color, size, focused}) => {
 
